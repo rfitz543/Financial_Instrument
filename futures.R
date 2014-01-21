dad8e5250781296060a6ed76fa4c451d9d3e5a99
rfitz543 <rfitz543@gmail.com>
2014-01-21 10:19:29 -0500

    added gold

--- futures.R
+++ futures.R
@@ -7,3 +7,9 @@ future('CO', 'USD', multiplier=1000, tick_size=.01)
 future('NG', 'USD', multiplier=10000, tick_size=.001)
 future('HO', 'USD', multiplier=42000, tick_size=.0001)
 future('RB', 'USD', multiplier=42000, tick_size=.0001)
+
+future('GC', 'USD', multiplier=100, tick_size=.1)
+
+saveInstruments('MyInstruments.RData')
+ls('package:FinancialInstrument')
+
